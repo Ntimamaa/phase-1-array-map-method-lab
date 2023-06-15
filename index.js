cc0f27073436b3c418fdbddb8102cabb3117f1cc
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-06-15 16:24:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  const titleCase = tutorials.map(tutorial => {
+    const words = tutorial.split(' ');
+    const capitalizedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return capitalizedWords.join(' ');
+  });
+
+  return titleCase;
 }
+
+console.log(titleCased());
